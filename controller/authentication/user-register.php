f025757ba2d1718167f816d24cc7ec30f494e1a3
Nasrul Wahabi <wahabinasrul@gmail.com>
2024-05-23 17:07:52 +0700

    chore: Update user-register.php and register.js

--- controller/authentication/user-register.php
+++ controller/authentication/user-register.php
@@ -1,20 +1,12 @@
 <?php
 
-include '../transactionApp/database/connection.php';
+include '../../database/connection.php';
 
 if (!isset($_POST['email'], $_POST['password'], $_POST['confirm-password'])) {
     exit('Please complete the registration form!');
 }
 
-if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-    exit('Email is not valid!');
-}
-
-if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
-    exit('Password must be between 5 and 20 characters long!');
-}
-
-if ($stmt = $con->prepare('SELECT email FROM users WHERE email = ?')) {
+if ($stmt = $db->prepare('SELECT email FROM users WHERE email = ?')) {
     // Bind parameters (s = string, i = int, b = blob, etc)
     $stmt->bind_param('s', $_POST['email']);
     $stmt->execute();
@@ -22,12 +14,18 @@ if ($stmt = $con->prepare('SELECT email FROM users WHERE email = ?')) {
     if ($stmt->num_rows > 0) {
         echo 'Email exists, please choose another!';
     } else {
-        if ($stmt = $con->prepare('INSERT INTO users (id_user, email, password) VALUES (?, ?, ?)')) {
+        if ($stmt = $db->prepare('INSERT INTO users (id_user, email, password) VALUES (?, ?, ?)')) {
             // encrypt the password
             $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
             $stmt->bind_param('iss', $_POST['id_user'], $_POST['email'], $password);
             $stmt->execute();
-            echo 'You have successfully registered! You can now login!';
+            // register the user is successful
+            echo "<script>";
+
+            // echo "alert('You have successfully registered, you can now login!');";
+            
+            // echo "location.href = '../../view/test/register.php';";
+            echo "</script>";
         } else {
             echo 'Could not prepare statement!';
         }
@@ -36,4 +34,4 @@ if ($stmt = $con->prepare('SELECT email FROM users WHERE email = ?')) {
 } else {
     echo 'Could not prepare statement!';
 }
-$con->close();
+$db->close();
